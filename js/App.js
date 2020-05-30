97b6264f103a8b52adc687977294c8dd1bd167ca
Gergely Nagy <preszthegas@gmail.com>
2020-05-31 00:30:04 +0200

    Messing around with the layout, setting icon based on the data, using metric unit instead of Kelvin, so a lot of converting is useless

--- js/App.js
+++ js/App.js
@@ -2,27 +2,18 @@ let inputButton = document.querySelector("#search-btn");
 let searchedCity = document.querySelector("#searched-city");
 let currentTemperature = document.querySelector("#current-temperature");
 let temperatureBtn = document.querySelector("#temperature-btn");
-
+let currentIcon = document.querySelector("#current-icon");
 //  Temperature Conversion Formulaes
-let kelvinToCelsius = (temperature) => {
-  return Math.round(temperature - 273.15);
-};
 
-let kelvinToFahrenheit = (temperature) => {
-  return Math.round((temperature * 9) / 5 - 459.67);
-};
 
 let celsiusToFahrenheit = (temperature) => {
   return Math.round((temperature * 9) / 5 + 32);
 };
 
-let fahrenheitToCelsius = (temperature) => {
-  return Math.round(((temperature - 32) * 5) / 9);
-};
 
 inputButton.addEventListener("click", () => {
   let city = document.querySelector("#search-city").value;
-  const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c87b73968acdc814157d174a59ddd93c`;
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=c87b73968acdc814157d174a59ddd93c`;
   fetch(api)
     .then((response) => {
       return response.json();
@@ -30,18 +21,21 @@ inputButton.addEventListener("click", () => {
     .then((data) => {
       console.log(data);
       let temp = data.main.temp;
-      currentTemperature.innerText = kelvinToCelsius(temp) + "°C";
+      currentTemperature.innerText = Math.round(temp) + "°C";
       searchedCity.innerText = data.name + ", " + data.sys.country;
       //  ToDo:
       //    [] Make prettier code
-      temperatureBtn.addEventListener("click", () => {
+      currentTemperature.addEventListener("click", () => {
         let searchCRegEx = /°C/;
         if (currentTemperature.innerText.match(searchCRegEx)) {
-          currentTemperature.innerText = celsiusToFahrenheit(kelvinToCelsius(temp)) + "°F";
+          currentTemperature.innerText = celsiusToFahrenheit(temp) + "°F";
         } else {
-          currentTemperature.innerText = fahrenheitToCelsius(kelvinToFahrenheit(temp)) + "°C";
+          currentTemperature.innerText = Math.round(temp) + "°C";
         }
       });
+      let iconCode = data.weather[0].icon;
+      const iconURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      currentIcon.setAttribute("src", iconURL);
     }).catch((err) => {
       console.warn("Something went wrong " + err);
     });
